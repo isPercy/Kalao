7f53b916b45dda3c3ae63613395eb5224843b8ca
isPercy <jpablo.godoyb@gmail.com>
2022-06-08 22:42:03 -0400

    base de datos señalada

--- backend/database.php
+++ backend/database.php
@@ -8,29 +8,30 @@ class Database{
     private $password;
     private $charset;
 
-    public function _contruct()
-    {
-        $this->host = 'localhost'
-        $this->db = 'blog'
-        $this->user = 'root'
-        $this->password = ''
-        $this->charset = 'utf8mb4'
+    public function __construct(){
+        $this->host = 'localhost';
+        $this->db = 'kalao';
+        $this->user = 'root';
+        $this->password = '';
+        $this->charset = 'utf8mb4';
     }
 
-    function connect()
-    {
-       try
-       {
-           $connection = "mysql:host=" . $this->host . ";dbname" . $this->db
-           $options = [
-               PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-               PDO::ATTR_EMULATE_PREPARES => false,
-           ];
-
-           $pdo = new PDO($connection, $this->user, $this->password, $options)
-       }
-       catch(PDOException $e){
-           print_r('Error connection: ' . $e->getMessage());
-       }
+    function connect(){
+        try{
+            $connection = "mysql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset;
+            $options = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
+            
+            $pdo = new PDO($connection, $this->user, $this->password, $options);
+    
+            return $pdo;
+        }catch(PDOException $e){
+            print_r('Error connection: ' . $e->getMessage());
+        }
     }
+
 }
+
+?>s
--- backend/join.php
+++ backend/join.php
@@ -3,84 +3,77 @@
 
     session_start();
 
-    if(isset($_GET['cerrar_sesion']))
-    {
+    if(isset($_GET['cerrar_sesion'])){
         session_unset();
+
         session_destroy();
     }
 
-    if(isset($_SESSION['rol']))
-    {
-        switch($_SESSION['rol'])
-        {
+    if(isset($_SESSION['rol'])){
+        switch($_SESSION['rol']){
             case 1:
-                header('location: Visitante')
+                header('location: ../frontend/categoria.html');
             break;
 
             case 2:
-                header('location: Usuario')
-            break;
-            
-            case 3:
-                header('location: Cliente')
-            break;
-
-            case 4:
-                header('location: Moderador')
+                header('location: ../frontend/carro.html');
             break;
 
             case 5:
-                header('location: Admin.php')
+                header('location: ../frontend/inicio.html');
             break;
 
             default:
         }
     }
 
-    if(isset($_POST['Correo']) && isset($_POST['Pass']))
-    {
+    if(isset($_POST['Correo']) && isset($_POST['Pass'])){
         $Correo = $_POST['Correo'];
         $Pass = $_POST['Pass'];
 
         $db = new Database();
         $query = $db->connect()->prepare('SELECT*FROM usuario WHERE Correo = :Correo AND Pass = :Pass');
-        $query->execute(['Correo' => $Correo, 'Pass' => $Pass]);
+        $query->execute(['Correo' => $Correo, 'Pass' => $Psass]);
 
-        $row = $query->fetch(PDO::FET_NUM);
-        if($row == true)//validar roll
-        {
-            $rol= $row[3]
+        $row = $query->fetch(PDO::FETCH_NUM);
+        if($row == true){
+            // validar rol
+            $rol = $row[3];
             $_SESSION['rol'] = $rol;
-            
-            switch($_SESSION['rol'])
-        {
-            case 1:
-                header('location: Visitante.php')
-            break;
 
-            case 2:
-                header('location: Usuario.php')
-            break;
-            
-            case 3:
-                header('location: Cliente.php')
-            break;
-
-            case 4:
-                header('location: Moderador.php')
-            break;
-
-            case 5:
-                header('location: Admin.php')
-            break;
-
-            default:
-        }
+            switch($_SESSION['rol']){
+                case 1:
+                    header('location: inicio.html');
+                break;
+    
+                case 5:
+                header('location: carro.html');
+                break;
+    
+                default:
+            }
+        }else{
+            // no existe el usuario
+            echo "El usuario o contraseña son incorrectos";
         }
 
-        else{//no existe el usuario
-            echo "El correo y contraseña no coinciden"
-        }
     }
+    
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+    <form action="#" method="POST">
+        correo: <br/><input type="text" name="correo"><br/>
+        Password: <br/><input type="text" name="password"><br/>
+        <input type="submit" value="Iniciar sesión">
+    </form>
+</body>
+</html>
